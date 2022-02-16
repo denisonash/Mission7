ad72122c6787bdbdbce0a19b75dcc12e5e4fac6c
ashleydenison <dashley9@byu.edu>
2022-02-16 10:45:16 -0700

    Final Commit

--- Mission7/Startup.cs
+++ Mission7/Startup.cs
@@ -5,18 +5,39 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Mission7.Models;
 
 namespace Mission7
 {
     public class Startup
     {
+
+        public IConfiguration Configuration { get; set; }
+
+        public Startup (IConfiguration temp)
+        {
+            Configuration = temp;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Tells it to use MVC pattern
+            services.AddControllersWithViews();
+
+            services.AddDbContext<BookstoreContext>(options =>
+            {
+                options.UseSqlite(Configuration["ConnectionStrings:BooksDBConnection"]);
+            });
+
+            services.AddScoped<IBookstoreRepository, EFBookstoreRepository>();
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -26,14 +47,13 @@ namespace Mission7
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
